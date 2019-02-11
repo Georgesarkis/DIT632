834bc16b75bc3aed2594027b907db11bc87d43be
George Sarkisian <sakogeorge@gmail.com>
2019-02-11 15:05:31 +0100

    exe 3 -3

--- exe3/Exerc_3_3.c
+++ exe3/Exerc_3_3.c
@@ -33,37 +33,55 @@ int main(int argc, char *argv[]){
     REGTYPE *akt_post , *head=NULL;
     srand( time(0));  //   Random seed 
     head=random_list();
-  /*   akt_post=head;
+    akt_post=head;
     while( akt_post!=NULL){
         printf("\n Post nr %d : %d" , nr++, akt_post -> number);
-        akt_post = akt_post -> next;
-    }  
-
-  /* ---Free of allocated memory ---
+        akt_post = akt_post -> prev;
+    }
+    nr=0;
+    int input;
+    printf("input a number to add to link:");
+    scanf("%d", &input );
+    head = add_first(head, input);
+    akt_post=head;
+    while( akt_post!=NULL){
+        printf("\n Post nr %d : %d" , nr++, akt_post -> number);
+        akt_post = akt_post -> prev;
+    }    
+  // ---Free of allocated memory ---
   while((akt_post=head)!=NULL){
       head=akt_post->next;
-      //free(akt_post);
+      free(akt_post);
     }  
-*/
+
     return 0;
 }
 
-
 REGTYPE* random_list(void ){
-    
-    REGTYPE *top, *old, *item;
-    int rnadom = rand() % 100;
-    printf("%d" , rnadom);
-    top ->number = rnadom;
-     printf("%d" , top -> number);
-    top ->next = NULL;
-    top ->prev = NULL;
-
+    int nr = MAX;
+    REGTYPE *top = (REGTYPE*)malloc(sizeof(REGTYPE));
+    top ->number = rand() % 100;
+    top->next = NULL;
+    top-> prev = NULL;
+    for(int i = 0 ; i < nr -1; i++){
+        REGTYPE * link = (REGTYPE*)malloc(sizeof(REGTYPE));
+        link -> number = rand()%100;
+        top -> next = link;
+        link -> prev= top;
+        link -> next = NULL;
+        top = link;   
+    }
     return(top);
 }
 
 
 //==========================================================
 REGTYPE* add_first(REGTYPE* temp, int data){
-    // Adds a record first i list and set the field tal to data}
+        REGTYPE * link = (REGTYPE*)malloc(sizeof(REGTYPE));
+        link -> number = data;
+        temp -> next = link;
+        link -> prev= temp;
+        link -> next = NULL;
+        temp = link;  
+    return(temp);
 }
