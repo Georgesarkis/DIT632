9cc2fa6627f6656e84d5b8e0707f0525c86490e5
MartinStanchev <stanchev.martin@gmail.com>
2019-02-26 16:37:50 +0100

    finished exe5

--- exe5/test.ino/test.ino.ino
+++ exe5/test.ino/test.ino.ino
@@ -5,16 +5,18 @@
 #define E 5
 #define F 6
 #define G 7
-#define D1 11
-#define D2 12
+#define D1 8
+#define D2 9
 
 const int segs[7] = { A, B, C, D, E, F, G };
 const byte numbers[10] = { 0b0111111, 0b00000110, 0b1011011, 0b1001111, 0b1100110, 0b1101101,
 0b1111101, 0b0000111, 0b1111111, 0b1101111 };
 
-
+int a, b;
+int max;
 
 void setup() {
+  pinMode(0 , OUTPUT);
   pinMode(1 , OUTPUT);
   pinMode(2 , OUTPUT);
   pinMode(3 , OUTPUT);
@@ -22,35 +24,36 @@ void setup() {
   pinMode(5 , OUTPUT);
   pinMode(6 , OUTPUT);
   pinMode(7 , OUTPUT);
-  pinMode(11 , OUTPUT);
-  pinMode(12, OUTPUT);
+  pinMode(8 , OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(13 , INPUT);
+  pinMode(12, INPUT);
+  //pinMode(13, OUTPUT);
   Serial.begin(9600);
 
 }
 
 void loop() {
-  
-  
-  int a = 0;
-  
+
   float val = analogRead(0);
-  int temp = ((val / 1024) * 5000) / 100;
-  int max;
-  
+  int temp = ((val / 1024) * 5000) / 10;
+
   if(max < temp) {
         max = temp;
   }
-  
-    a = digitalRead(8);
-    
-    if(a == 1) {
+
+    a = digitalRead(13);
+    b = digitalRead(12);
+    while(a == LOW && b == LOW){
+      a = digitalRead(13);
+      b = digitalRead(12);
       Serial.println("dip switch on");
       Serial.println(max);
-      writeDigit1(numbers[ (int)(max % 10) ]);
-      //delay(5);
-      writeDigit2(numbers[ (int)(max / 10) ]);
-      //delay(5);
-    } else {
+      writeDigit2(numbers[ (int)(max % 10) ]);
+      delay(5);
+      writeDigit1(numbers[ (int)(max / 10) ]);
+      delay(5);
+    } //else {
       Serial.println("dip switch off");
   
       
@@ -66,7 +69,7 @@ void loop() {
     delay(5);
     writeDigit1(numbers[ (int)(temp / 10)] );
     delay(5);
-  }
+  //}
   }
   }
 
