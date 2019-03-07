102adef61428a33bfa8b9d4e76f3757fa00a32a3
George Sarkisian <sakogeorge@gmail.com>
2019-03-07 15:03:58 +0100

    exe 6

--- exe6/Exerc_6_4alt.c
+++ exe6/Exerc_6_4alt.c
@@ -6,8 +6,14 @@ int program_time = 0; // The global time, start value 0
 void *time_count();
 void *read_inport();
 double get_time_ms();
+pthread_mutex_t lock;
 
 int main(){
+    if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("\n mutex init failed\n");
+        return 1;
+    }
  // Start up the thread time_count.
     pthread_t thread_id; 
     pthread_create(&thread_id, NULL, time_count, NULL); 
@@ -20,10 +26,13 @@ int main(){
          
     //Print out system time every second.
         if(timeNow != program_time){
-            timeNow == program_time;
+            timeNow = program_time;
             printf("%d\n"  ,program_time);
         }
     }
+    pthread_join(thread_id, NULL);
+    pthread_join(thread_id2, NULL);
+    pthread_mutex_destroy(&lock);
 }
 // --- End of main thread ------
 // ----- Tread functions –
@@ -35,17 +44,26 @@ void *time_count() {
     // Increase program_time by one every second.
         if(get_time_ms() - timeNow == 1){
             timeNow = get_time_ms() ;
+            pthread_mutex_lock(&lock);
             program_time++;
+            pthread_mutex_unlock(&lock);
+
         }    
     }
 // exit thread;
     pthread_exit(NULL); 
 } //End------------------------------------
 void *read_inport(){
+    int timeNow = program_time;
+
     while (program_time<50){
-        if(program_time % 5 == 0){
-            printf("read import");
+        pthread_mutex_lock(&lock);
+        if(program_time % 5 == 0 && program_time != 0 && timeNow != program_time){
+            timeNow = program_time;
+            printf("programtime: %d\n" , program_time);   
+            printf("read import\n");   
         }   
+        pthread_mutex_unlock(&lock);
     }
     //Exit thread
     pthread_exit(NULL); 
---
+++ exe6/Exerc_6_5alt.c
@@ -0,0 +1,99 @@
+// Skeleton for exercise nr 6 in WP 6 course DIT165.
+// File skeleton_wp6_6.c  
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+pthread_mutex_t count_mutex     = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t  not_empty   = PTHREAD_COND_INITIALIZER;
+pthread_cond_t  not_full   = PTHREAD_COND_INITIALIZER;
+
+// Global buffer and corresponding counters.
+char  letter = 'a'; //the starting letter
+#define MAX 10//buffer size
+unsigned short count = 0;
+char buffer[MAX]; // circular buffer
+int write_pos = 0; // index for next character to be put in buffer (put)
+int read_pos = 0; // index for next character to be read ( fetch )
+
+void *put();
+void *fetch();
+
+int main(){
+    int i;
+	pthread_t tid, tid2;
+
+	pthread_create(&tid, NULL, put, NULL);
+
+
+	pthread_create(&tid2, NULL, fetch, NULL);
+   while(1){
+       if(strlen(buffer) != MAX){
+            pthread_cond_signal(&not_full);
+
+       }
+       if(strlen(buffer) != 0){
+            pthread_cond_signal(&not_empty);
+
+       }
+		
+   }
+
+	pthread_join(tid, NULL);
+    pthread_join(tid2, NULL);
+    return 0;
+}
+
+
+void *put(){
+    char input = 97;
+    int pos = 0;
+    while(1){
+        pthread_mutex_lock( &count_mutex );
+        pthread_cond_wait(&not_full, &count_mutex);
+        
+        buffer[write_pos] = input;
+        pthread_cond_signal(&not_empty);
+
+        if(write_pos > MAX){
+            write_pos = 0;
+        }else{
+                write_pos ++; 
+        }
+        if(input > 121){
+            input = 97;
+        }
+        else{
+            input++;
+        }
+        pthread_mutex_unlock( &count_mutex );
+	
+    }
+   	pthread_exit(0);
+
+}
+
+
+void *fetch(){
+    while(1){
+
+
+		pthread_mutex_lock( &count_mutex );
+                        pthread_cond_wait(&not_empty, &count_mutex);
+        printf("%c", buffer[read_pos]);
+        buffer[read_pos] = 0;
+        pthread_cond_signal(&not_full);
+
+        if(read_pos > MAX){
+            read_pos = 0;
+            printf("\n");
+        }else{
+            read_pos++; 
+        }
+        pthread_mutex_unlock( &count_mutex );
+
+    }
+	pthread_exit(0);
+
+}
